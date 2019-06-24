205a61afbca61878a46429c953c87d4bf9002d32
Rafy benjamin <rafy.amgadbenjamin@hotmail.com>
2019-06-24 23:12:05 +0200

    -Creating searchCharacter API URL://character/<name> -Create character model

--- star wars app/star_wars_app/views.py
+++ star wars app/star_wars_app/views.py
@@ -6,35 +6,33 @@ from datetime import datetime
 from flask import render_template
 from star_wars_app import app
 from flask import request
+import character
 import swapi
+import requests 
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
+    return render_template('index.html',
         title='Home Page',
-        year=datetime.now().year,
-    )
+        year=datetime.now().year,)
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
+    return render_template('contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
-    )
+        message='Your contact page.')
 
 @app.route('/characters')
 def characters():
     """Renders the about page."""
     #Integrate with SWAPI API
     characters = swapi.get_all("people")
-    return render_template(
-        'characters.html',
+    return render_template('characters.html',
         title='characters',
         characters=characters.items)
 
@@ -42,9 +40,18 @@ def characters():
 @app.route('/single-character')
 def getsingleCharacterInfo():
 
-    #//reponse = requests.get('https://swapi.co/api/people/1/')
     singleCharacter = request.args.get('character', None)
-
+    
     return render_template('character.html',
                            title = "star wars character",
                            data=singleCharacter)
+
+
+@app.route('/character/<name>')
+def searchCharacter(name):
+    #endPointUrl = "https://swapi.co/api/people/?search=" + name
+    #reponse = requests.get(endPointUrl)
+    url = 'https://swapi.co/api/people/'
+    reponse = requests.get(url, params={'search': name})
+    return reponse
+
