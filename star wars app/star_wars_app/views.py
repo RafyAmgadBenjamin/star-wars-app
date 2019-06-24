5b8d45b45e9515feb6420f627cf626e51359edbe
Rafy benjamin <rafy.amgadbenjamin@hotmail.com>
2019-06-24 21:05:27 +0200

    -Integrate with SWAPI to get all characters using API -Create Single Character page and API

--- star wars app/star_wars_app/views.py
+++ star wars app/star_wars_app/views.py
@@ -5,6 +5,8 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from star_wars_app import app
+from flask import request
+import swapi
 
 @app.route('/')
 @app.route('/home')
@@ -29,7 +31,20 @@ def contact():
 @app.route('/characters')
 def characters():
     """Renders the about page."""
+    #Integrate with SWAPI API
+    characters = swapi.get_all("people")
     return render_template(
         'characters.html',
         title='characters',
-        year=datetime.now().year)
+        characters=characters.items)
+
+
+@app.route('/single-character')
+def getsingleCharacterInfo():
+
+    #//reponse = requests.get('https://swapi.co/api/people/1/')
+    singleCharacter = request.args.get('character', None)
+
+    return render_template('character.html',
+                           title = "star wars character",
+                           data=singleCharacter)
