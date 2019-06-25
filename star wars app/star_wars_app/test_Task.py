6bf23b14d795546c23d8f50c578f3b845e5fff9b
Rafy benjamin <rafy.amgadbenjamin@hotmail.com>
2019-06-25 19:22:10 +0200

    Apply integration test for  searchCharacter API URL: /api/character/<name>

--- star wars app/star_wars_app/test_Task.py
+++ star wars app/star_wars_app/test_Task.py
@@ -1,20 +1,13 @@
 import unittest
+from star_wars_app import app
 
-class TestStringMethods(unittest.TestCase):
+class TestIntegrations(unittest.TestCase):
+    def setUp(self):
+        self.app= app.test_client()
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_searchCharacterSuccessScenario(self):
+        response = self.app.get('/api/character/luke')
+        self.assertEqual(response._status_code,200)
 
 if __name__ == '__main__':
     unittest.main()
