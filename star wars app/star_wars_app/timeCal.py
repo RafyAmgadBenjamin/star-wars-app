b16ccd8f10729312b3167f51a78100c7421dcb78
Rafy benjamin <rafy.amgadbenjamin@hotmail.com>
2019-06-25 23:39:58 +0200

    Update the identation in timeCal

--- star wars app/star_wars_app/timeCal.py
+++ star wars app/star_wars_app/timeCal.py
@@ -1,6 +1,7 @@
 class timeCal(object):
     """This class is used to save the time to be able to parse it to json """
-    def __init__(self,time):
-                self.time = time
+    def __init__(self, time):
+        self.time = time
+                
 
 
